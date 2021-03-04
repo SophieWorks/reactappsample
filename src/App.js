7981e069440788ec07bee400d97a6dfa4eca1c61
SophieWorks <sophiework05@gmail.com>
2021-03-03 23:59:11 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './sayHello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){ 
+
+  return(
+    <div>
+      <h1>Hello React! new</h1>
+      <h1>This is the App Component</h1>
+      <Hello />
     </div>
   );
+
 }
 
-export default App;
+export default App; //exports function to the 'root'
